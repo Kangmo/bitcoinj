4c419efb7cba66b61c274514375d96badfceae0f
kangmo <kangmo.kim@gmail.com>
2014-09-07 11:27:55 +0900

    Ported Monetary. mvn test passed.

--- core/src/main/scala/com/nhnsoft/bitcoin/core/Monetary.scala
+++ core/src/main/scala/com/nhnsoft/bitcoin/core/Monetary.scala
@@ -22,18 +22,18 @@ import java.io.Serializable;
 /**
  * Classes implementing this interface represent a monetary value, such as a Bitcoin or fiat amount.
  */
-public interface Monetary extends Serializable {
+trait Monetary extends Serializable {
 
     /**
      * Returns the absolute value of exponent of the value of a "smallest unit" in scientific notation. For Bitcoin, a
      * satoshi is worth 1E-8 so this would be 8.
      */
-    int smallestUnitExponent();
+    def smallestUnitExponent() : Int
 
     /**
      * Returns the number of "smallest units" of this monetary value. For Bitcoin, this would be the number of satoshis.
      */
-    long getValue();
+    def getValue() : Long
 
-    int signum();
+    def signum() : Int
 }
